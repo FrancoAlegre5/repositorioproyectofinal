fd0a349ff62d7bb72ae7951f2d365eb6ebf3e35c
FrancoAlegre5 <146146086+FrancoAlegre5@users.noreply.github.com>
2023-09-27 21:10:46 -0300

    Add files via upload

---
+++ miproyectofinal/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="mi_packredist",
+    version="1.0",
+    description="packredist",
+    author="Franco Alegre",
+    author_email="francoalegre1999@gmail.com",
+    packages=["Cliente"]
+)
